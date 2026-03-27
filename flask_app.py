a24942b387e1272e4c6e319fd83f19bdab6a340b
UnicornBog <avaelam35@gmail.com>
2026-03-27 07:49:33 -0500

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -33,22 +33,23 @@ def index():
     </body>
     '''
 #Second Page
-@app.route("/page")
+@app.route("/results", method="POST")
 def page():
-    return '''
-    <head>
-     <style>
-            h1{
-                color:red
-            }
-            body{
-                background-color: blue;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Hello page<h1>
-    </body>
+    color_choice = request.form['color']
+    fav_num = request.form['luck_num']
+    fav_animal = request.form['fav_class']
+    fav_movie = request.form['best_pix']
+
+    return render_template('form_results.html', color = color_choice, luck_num = fav_num, fav_class = fav_animal, best_pix = fav_movie)
+    '''
+    <form action="/results" method="POST">
+        <h2>Favorite Things</h2>
+        <label>Favorite Color: <input type="text" name="color"/></label><br>
+        <label>Favorite Number: <input type="number" name="luck_num"/></label><br>
+        <label>Favorite Animal: <input type="text" name="fav_class"/></label><br>
+        <label>Favorite Movie: <input type="text" name="best_pix"/></label><br>
+        <button>Submit</button>
+    </form>
     '''
  #third page
 @app.route("/apple")
